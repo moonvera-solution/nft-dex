497c47fee0371e6f114262120836bae8e39b5d07
Daniel G <danielifguerrero@gmail.com>
2023-10-01 23:19:16 -0500

    add uups of template artCollection

--- script/Deployer.s.sol
+++ script/Deployer.s.sol
@@ -4,6 +4,10 @@ pragma solidity ^0.8.5;
 import {Script, console2} from "forge-std/Script.sol";
 import {Factory} from "../src/Factory.sol";
 import {ArtCollection} from "../src/ArtCollection.sol";
+import {ERC1967Proxy} from "openzeppelin-contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import "../test/TestSetUp.sol";
+
+
 import "./Utils.sol";
 
 contract Deployer is Script, Utils {
@@ -22,20 +26,9 @@ contract Deployer is Script, Utils {
         vm.stopBroadcast();
     }
 
-    function _run() external {
-        console.log("GOERLI_RPC_URL", GOERLI_RPC_URL);
-        uint256 forkId = vm.createFork(GOERLI_RPC_URL, BLOCK_NUM);
-        vm.selectFork(forkId);
-
-        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_KEY");
-        address regularMinter = vm.addr(vm.envUint("REG_MINTER_KEY"));
-
-        vm.startBroadcast(deployerPrivateKey);
-        Factory factory = new Factory(3000);
-        ArtCollection template = new ArtCollection();
-
-        factory.setCollectionImpl(address(template));
-        factory.updateMember(regularMinter, 5); // 5 days to deploy artCollection
-        vm.stopBroadcast();
+    /// @notice this is unic UUPS proxy of template not a minimal clone
+    function deployTemplateProxy() internal {
+        address implementation = address(new ArtCollection());
+        address proxy = address(new ERC1967Proxy(implementation));
     }
 }
