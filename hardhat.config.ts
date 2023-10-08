1623b9bf4ac517043fe105de93883c7243025345
Daniel G <danielifguerrero@gmail.com>
2023-10-07 21:25:44 -0500

    fix ci

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,30 @@
-import { HardhatUserConfig } from "hardhat/config";
+import {HardhatUserConfig} from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as tdly from "@tenderly/hardhat-tenderly";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+tdly.setup({automaticVerifications: true})
+
+const {
+    TENDERLY_ACCESS_KEY,
+    TENDERLY_PROJECT_SLUG,
+    DEVNET_RPC_URL,
+} = process.env;
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+    solidity: "0.8.18",
+    networks: {
+        tenderly: {
+            url: DEVNET_RPC_URL,
+            chainId: 1,
+        }
+    },
+    tenderly: {
+        project: TENDERLY_PROJECT_SLUG || "devnet-example",
+        username: "danielleseng",
+        accessKey: TENDERLY_ACCESS_KEY,
+    }
 };
 
 export default config;
